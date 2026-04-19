24cb65cd80f9d5aa223530da5d7d26b4bdbc3ec5
Sneha Rajput <sneharajput7389@gmail.com>
2026-04-19 22:45:59 +0530

    file add

---
+++ 2.ConditionalStatement/minimumOfNo.java
@@ -0,0 +1,12 @@
+public class minimumOfNo {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        if(a < b){
+            System.out.print("a is smallest");
+        }
+        else{
+            System.out.print("b is smallest");
+        }
+    }
+}
